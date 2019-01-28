09022c04d5425fd6afbf0926c6ef99a931e416e8
RottenFruits <s_ogawa@akarf.com>
2019-01-28 16:19:39 +0900

    no message

--- atcorder/AtCoder_Beginners Selection/PracticeA.jl
+++ atcorder/AtCoder_Beginners Selection/PracticeA.jl
@@ -1,19 +1,16 @@
 function main()
-    #a = "1"
-    #b_c = "2 3"
-    #st = "test"
-
     a = readline()
     b_c = readline()
     st = readline()
 
-    b_c = split(b_c, " ")
+    b, c = split(b_c, " ")
 
     a = parse(Int, a)
-    b = parse(Int, b_c[1])
-    c = parse(Int, b_c[2])
+    b = parse(Int, b)
+    c = parse(Int, c)
 
-    print(a + b + c, " ", st)
+    #print(a + b + c, " ", st)
+    print("$(a + b + c) $st")
 end
 
 main()
