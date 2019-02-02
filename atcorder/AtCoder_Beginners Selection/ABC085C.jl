45e26c98da64826f3a9b882471edecdd3827e5b5
RottenFruits <s_ogawa@akarf.com>
2019-02-02 15:05:48 +0900

    no message

--- atcorder/AtCoder_Beginners Selection/ABC085C.jl
+++ atcorder/AtCoder_Beginners Selection/ABC085C.jl
@@ -3,7 +3,7 @@ function f()
     n = input[1]
     y = input[2]
 
-    for i in 0:n, j in 0:n
+    for i in 0:n, j in 0:(n - i)
         if i + j <= n
             y1 = 1000 * i
             y2 = 5000 * j
