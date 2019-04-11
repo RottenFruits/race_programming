2f4bb7fb749c9fe8c6123a4320b9428e0ded9e28
RottenFruits <s_ogawa@akarf.com>
2019-04-11 20:13:05 +0900

    none

--- atcorder/AtCoder Beginner Contest 006/a.jl
+++ atcorder/AtCoder Beginner Contest 006/a.jl
@@ -1,4 +1,10 @@
 function f()
-    print()
+    N = parse(Int, chomp(readline()))
+
+    if N % 3 == 0 || N == 3
+        print("YES")
+    else
+        print("NO")
+    end
 end
 f()
--- atcorder/AtCoder Beginner Contest 006/b.jl
+++ atcorder/AtCoder Beginner Contest 006/b.jl
@@ -1,4 +1,12 @@
 function f()
-    print()
+    n = parse(Int, chomp(readline()))
+
+    a = [0, 0, 1]
+
+    for i in 4:n
+        append!(a, sum(a[i-3:end]) % 10007)
+    end
+
+    print(a[n])
 end
 f()
