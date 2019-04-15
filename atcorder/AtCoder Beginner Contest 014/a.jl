2357793fc13fb1cb3f68e875e62dd667a66b6859
RottenFruits <s_ogawa@akarf.com>
2019-04-15 21:35:17 +0900

    none

--- atcorder/AtCoder Beginner Contest 014/a.jl
+++ atcorder/AtCoder Beginner Contest 014/a.jl
@@ -1,4 +1,15 @@
 function f()
-    print()
+    a = parse(Int, chomp(readline()))
+    b = parse(Int, chomp(readline()))
+    
+    amari = a % b
+    
+    if amari == 0
+        ret = 0
+    else
+        ret = b - amari
+    end
+
+    println(ret)
 end
 f()
--- atcorder/AtCoder Beginner Contest 014/b.jl
+++ atcorder/AtCoder Beginner Contest 014/b.jl
@@ -1,4 +1,15 @@
 function f()
-    print()
+    input = map(x -> parse(Int, x), split(chomp(readline()), " "))
+    n = input[1]
+    X = input[2]
+    a = map(x -> parse(Int, x), split(chomp(readline()), " "))
+
+    b = bits(X)[end-n+1:end]
+
+    ret = 0
+    for i in 1:length(b)
+        ret += parse(Int, b[length(b) - i + 1]) * a[i]
+    end
+    println(ret)
 end
 f()
