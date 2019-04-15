c1b2c456bb7b56b6a0fa85917b4b039ae7441c73
RottenFruits <s_ogawa@akarf.com>
2019-04-15 21:06:31 +0900

    none

--- atcorder/AtCoder Beginner Contest 013/a.jl
+++ atcorder/AtCoder Beginner Contest 013/a.jl
@@ -1,4 +1,16 @@
 function f()
-    print()
+    X = chomp(readline())
+
+    if X == "A"
+        println(1)
+    elseif X == "B"
+        println(2)
+    elseif X == "C"
+        println(3)
+    elseif X == "D"
+        println(4)
+    elseif X == "E"
+        println(5)
+    end
 end
 f()
--- atcorder/AtCoder Beginner Contest 013/b.jl
+++ atcorder/AtCoder Beginner Contest 013/b.jl
@@ -1,4 +1,11 @@
 function f()
-    print()
+    a = parse(Int, chomp(readline()))
+    b = parse(Int, chomp(readline()))
+
+    tmp1 = abs(b - a)
+    tmp2 = 10 - abs(b - a)
+
+    println(min(tmp1, tmp2))
+
 end
 f()
