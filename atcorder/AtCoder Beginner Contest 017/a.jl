88e678ec69d824ea14e64c18e84b0804545e2a3f
RottenFruits <s_ogawa@akarf.com>
2019-04-17 21:06:24 +0900

    none

--- atcorder/AtCoder Beginner Contest 017/a.jl
+++ atcorder/AtCoder Beginner Contest 017/a.jl
@@ -1,4 +1,11 @@
 function f()
-    print()
+    input1 = map(x -> parse(Int, x), split(chomp(readline()), " "))
+    input2 = map(x -> parse(Int, x), split(chomp(readline()), " "))
+    input3 = map(x -> parse(Int, x), split(chomp(readline()), " "))
+
+    i1 = input1[1] * (input1[2] / 10)
+    i2 = input2[1] * (input2[2] / 10)
+    i3 = input3[1] * (input3[2] / 10)
+    println(Int(i1+ i2+ i3))
 end
 f()
--- atcorder/AtCoder Beginner Contest 017/b.jl
+++ atcorder/AtCoder Beginner Contest 017/b.jl
@@ -1,4 +1,13 @@
 function f()
-    print()
+    s = chomp(readline())
+
+    if isempty(s)
+        println("YES")
+    elseif s[1:5] == "choku" && (s[end - 1:end] == "ch" || s[end] == 'o' || s[end] == 'k' || s[end] == 'u') 
+        println("YES")
+    else
+        println("NO")
+    end
+    
 end
 f()
