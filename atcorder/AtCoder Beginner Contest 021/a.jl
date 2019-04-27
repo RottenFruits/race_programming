0cb92345a820d83718199a88c2ea64b909534403
RottenFruits <s_ogawa@akarf.com>
2019-04-27 16:32:16 +0900

    none

--- atcorder/AtCoder Beginner Contest 021/a.jl
+++ atcorder/AtCoder Beginner Contest 021/a.jl
@@ -1,4 +1,43 @@
 function f()
-    print()
+    n = parse(Int, chomp(readline()))
+
+    if n == 1
+        println("1")
+        println("1")
+    elseif n == 2
+        println("1")
+        println("2")
+    elseif n == 3
+        println("2")
+        println("1")
+        println("2")
+    elseif n == 4
+        println("1")
+        println("4")
+    elseif n == 5
+        println("2")
+        println("1")
+        println("4")
+    elseif n == 6
+        println("2")
+        println("2")
+        println("4")
+    elseif n == 7
+        println("3")
+        println("4")
+        println("1")
+        println("2")
+    elseif n == 8
+        println("1")
+        println("8")
+    elseif n == 9
+        println("2")
+        println("8")
+        println("1")
+    elseif n == 10
+        println("2")
+        println("2")
+        println("8")
+    end
 end
 f()
--- atcorder/AtCoder Beginner Contest 021/b.jl
+++ atcorder/AtCoder Beginner Contest 021/b.jl
@@ -1,4 +1,18 @@
 function f()
-    print()
+    n = parse(Int, chomp(readline()))
+    input = map(x -> parse(Int, x), split(chomp(readline()), " "))
+    a = input[1]
+    b = input[2]
+    K = parse(Int, chomp(readline()))
+
+    P = map(x -> parse(Int, x), split(chomp(readline()), " "))
+
+    P_u = unique(P)
+
+    if length(P) != length(P_u) || any(P .== a) || any(P .== b)
+        println("NO")
+    else
+        println("YES")
+    end
 end
 f()
